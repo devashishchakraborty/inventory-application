45cbdfbac893235541270d58a2c1d7bf8c47e2e4
Devashish Chakraborty <devashishchakra@gmail.com>
2025-01-25 02:03:10 +0530

    Completed the addCategory section by adding appropriate validator for duplicate checking

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,6 +4,18 @@ import { body, validationResult } from "express-validator";
 import CustomNotFoundError from "../errors/CustomNotFoundError.js";
 import utils from "../Utils.js";
 
+const validateCategory = [
+  body("categoryName")
+    .toLowerCase()
+    .trim()
+    .custom(async (value) => {
+      const categories = await queries.getAllCategories();
+      const categorieNames = categories.map((category) => category.name);
+      if (categorieNames.includes(value)) {
+        throw new Error("Category already exists! Try a different one.");
+      }
+    }),
+];
 
 const getCategories = async (req, res) => {
   const categories = await queries.getAllCategories();
@@ -12,11 +24,21 @@ const getCategories = async (req, res) => {
 
 const addCategoryGet = (req, res) => res.render("addCategory");
 
-const addCategoryPost = async (req, res) => {
-  const { categoryName } = req.body;
-  await queries.insertCategory(categoryName);
-  res.redirect("/categories");
-};
+const addCategoryPost = [
+  validateCategory,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("addCategory", {
+        errors: errors.array(),
+      });
+    }
+
+    const { categoryName } = req.body;
+    await queries.insertCategory(categoryName);
+    res.redirect("/categories");
+  },
+];
 
 const getItemsByCategory = asyncHandler(async (req, res) => {
   const { categoryName } = req.params;
